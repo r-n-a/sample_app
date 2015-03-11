82b153a2d31e0907040666b6991c4c9f8f74bf96
Nic <r-n-a@ukr.net>
2015-03-11 20:36:37 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", 
-								email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -72,5 +74,14 @@ describe User do
 		  expect(@user.reload.email).to eq mixed_case_email.downcase
 		  end
 	end
+	
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
+	
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
 
 end
